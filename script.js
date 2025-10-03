eb95a9713871dc5bc6711ef8fcae7e47f43aa217
kurukshetra <kurukshetracodingschool.com>
2025-10-03 18:06:38 +0530

    Bubble game Complete

--- script.js
+++ script.js
@@ -1,40 +1,63 @@
 let panelbottom = document.querySelector('.panel-bottom')
 let timebox = document.querySelector('.time .box')
 let hitBox = document.querySelector('.hit .box')
+let scoreBox = document.querySelector('.score .box')
+
+let score = 0;
+let timerValue = 60;
+let interval;
+let hitRandom;
 
 
-function makebubble() {  
-let bubble = ''
-for (let i = 0; i<=132; i++){
-    let randomBubble = Math.floor(Math.random()*10)
-   bubble += `<div class="bubble">${randomBubble}</div>`
-}
-
-panelbottom.innerHTML = bubble
-}
-
-function timer (){
-    let timer = 60;
-setInterval(function(){
-     if(timer>0){
-     timer--
-     timebox.innerHTML = timer
-     }
-     else{
-        panelbottom.innerHTML = "<h1>Game Over !</h1>"
-     }
-    
-},1000)
-}
-
-function randomhit (){
-    let hitRandom = Math.floor(Math.random()*10)
-    hitBox.innerHTML =  hitRandom;
+function makebubble() {
+    let bubble = ''
+    for (let i = 0; i <= 132; i++) {
+        let randomBubble = Math.floor(Math.random() * 10)
+        bubble += `<div class="bubble">${randomBubble}</div>`
+    }
+    panelbottom.innerHTML = bubble
 }
 
 
+function timer() {
+    interval = setInterval(function () {
+        if (timerValue > 0) {
+            timerValue--
+            timebox.innerHTML = timerValue
+        } else {
+            clearInterval(interval)
+            panelbottom.innerHTML = "<h1>⏳ Game Over !</h1>"
+        }
+    }, 1000)
+}
 
+
+function randomhit() {
+    hitRandom = Math.floor(Math.random() * 10)
+    hitBox.innerHTML = hitRandom
+}
+
+
+panelbottom.addEventListener('click', (e) => {
+    if (e.target.classList.contains("bubble")) {
+        let bubble = Number(e.target.innerHTML)
+
+        if (bubble === hitRandom) {
+            score += 10
+            scoreBox.innerHTML = score
+            makebubble()    
+            randomhit()     
+        }
+        else {
+            
+            score -= 5
+            if (score < 0) score = 0
+            scoreBox.innerHTML = score
+        }
+    }
+})
+
+// game start
 randomhit()
 timer()
-
 makebubble()
