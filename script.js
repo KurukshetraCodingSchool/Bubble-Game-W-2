78277bf7ae1fb48a4fe953a06b1b27747899b67e
kurukshetra <kurukshetracodingschool.com>
2025-10-03 17:31:53 +0530

    hit and timer is work

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let panelbottom = document.querySelector('.panel-bottom')
+let timebox = document.querySelector('.time .box')
+let hitBox = document.querySelector('.hit .box')
 
+
+function makebubble() {  
 let bubble = ''
 for (let i = 0; i<=132; i++){
     let randomBubble = Math.floor(Math.random()*10)
@@ -7,3 +11,30 @@ for (let i = 0; i<=132; i++){
 }
 
 panelbottom.innerHTML = bubble
+}
+
+function timer (){
+    let timer = 60;
+setInterval(function(){
+     if(timer>0){
+     timer--
+     timebox.innerHTML = timer
+     }
+     else{
+        panelbottom.innerHTML = "<h1>Game Over !</h1>"
+     }
+    
+},1000)
+}
+
+function randomhit (){
+    let hitRandom = Math.floor(Math.random()*10)
+    hitBox.innerHTML =  hitRandom;
+}
+
+
+
+randomhit()
+timer()
+
+makebubble()
